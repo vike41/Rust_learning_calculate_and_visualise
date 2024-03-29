eec05b9f51bd2220b6baab83ebee94462d1c1c5f
vike41 <vitalikkens@gmail.com>
2024-03-29 13:13:16 +0100

    Part_1

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,87 @@ mod window;
 use window::{Window, Framebuffer};
 use std::io;
 
+use glam::*;
 
+fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
+    let (r, g, b) = (r as u32, g as u32, b as u32);
+    (r << 16) | (g << 8) | b
+}
+
+fn edge_function(a: &Vec2, c: &Vec2, b: &Vec2) -> f32 {
+    (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x)
+}
+
+fn inside_triangle(a: &Vec2, b: &Vec2, c: &Vec2, p: &Vec2) -> bool {
+    let a0 = edge_function(b, c, p);
+    let a1 = edge_function(c, a, p);
+    let a2 = edge_function(a, b, p);
+
+    let mut overlaps = true;
+    overlaps &= a0 > 0.0;
+    overlaps &= a1 > 0.0;
+    overlaps &= a2 > 0.0;
+
+    overlaps
+}
+
+fn draw_triangle(framebuffer: &mut Framebuffer, a: &Vec2, b: &Vec2, c: &Vec2, color: u32) {
+    let width = framebuffer.width();
+    let height = framebuffer.height();
+
+    let min = a.min(b.min(*c)).max(Vec2::ZERO) * Vec2::new(width as f32, height as f32);
+    let max = a.max(b.max(*c)).min(Vec2::ONE) * Vec2::new(width as f32, height as f32);
+
+    for x in (min.x as usize)..(max.x as usize) {
+        for y in (min.y as usize)..(max.y as usize) {
+            let p = Vec2::new(
+                x as f32 / width as f32,
+                y as f32 / height as f32
+            );
+
+            let inside = inside_triangle(a, b, c, &p);
+            if inside {
+                framebuffer.set_pixel(x, y, color);
+            }
+        }
+    }
+}
+
+static POINTS: &[Vec2] = &[
+    Vec2::new(0.3, 0.3),
+    Vec2::new(0.7, 0.3),
+    Vec2::new(0.5, 0.7),
+
+    Vec2::new(0.1, 0.3),
+    Vec2::new(0.5, 0.1),
+    Vec2::new(0.2, 0.6),
+
+    Vec2::new(0.5, 0.7),
+    Vec2::new(0.9, 0.7),
+    Vec2::new(0.5, 0.9),
+];
 
 fn main() {
-    let mut window:Window = Window::new_window("Cal a object", 512, 512);
+    let mut window = Window::new("3D graphics from scratch! (PART 1)", 256, 256);
 
-    while !window.close_window(){
-        let framebuffer = window.frame_buffer();
-        for x in 0..framebuffer.width(){
-            for y in 0..framebuffer.height(){
-                framebuffer.set_pixel(x,y,0);
-            }
+    while !window.should_close() {
+        let framebuffer = window.framebuffer();
+
+        framebuffer.clear(from_u8_rgb(20, 20, 20));
+
+        for i in 0..(POINTS.len() / 3) {
+            draw_triangle(
+                framebuffer,
+                &POINTS[i * 3],
+                &POINTS[i * 3 + 1],
+                &POINTS[i * 3 + 2],
+                from_u8_rgb((i * 100 + 100) as u8, 100, 50)
+            );
         }
 
         window.display();
     }
+}
 
     // // Define Circle
     // let mut input = String::new();
@@ -48,4 +114,4 @@ fn main() {
     // println!("Circles Diameter is {}, length is: {:.0}", diameter, circle_length);
     // println!("Area sector is: {:.0} and area Segment is: {:.0}", area_sector, area_segment);
 
-}
+
--- src/window.rs
+++ src/window.rs
@@ -1,71 +1,82 @@
-use minifb::WindowOptions;
-
-pub struct Window{
+pub struct Window {
     window: minifb::Window,
     framebuffer: Framebuffer
 }
 
-pub struct Framebuffer{
+pub struct Framebuffer {
     data: Vec<u32>,
     width: usize,
     height: usize
 }
 
 impl Window {
-    pub fn new_window(name: &str, screen_width: usize, screen_height: usize) -> Self{
-        let options: WindowOptions = minifb::WindowOptions{
+    pub fn new(name: &str, width: usize, height: usize) -> Self {
+        let options = minifb::WindowOptions {
             resize: true,
             ..Default::default()
         };
 
         let window = minifb::Window::new(
             name,
-            screen_width,
-            screen_height,
+            width,
+            height,
             options
-        ).expect("Failed to create window!");
+        ).expect("Failed to create window.");
 
-        Window{
+        let framebuffer = Framebuffer::new(width, height);
+
+        Window {
             window,
-            framebuffer:Framebuffer::new(screen_width, screen_height)
+            framebuffer
         }
     }
-    pub fn frame_buffer(&mut self) -> &mut Framebuffer{
-        &mut self.framebuffer
-    }
-    pub fn close_window (&self) -> bool {
+
+    pub fn should_close(&self) -> bool {
         !self.window.is_open()
     }
 
-    pub fn display(&mut self){
+    pub fn display(&mut self) {
         self.window.update_with_buffer(
             &self.framebuffer.data,
-            self.framebuffer.width,
-            self.framebuffer.height
-        ).expect("Failed to display window pixels!");
+            self.framebuffer.width(),
+            self.framebuffer.height()
+        ).expect("Failed to update window buffer.");
+
+        let (width, height) = self.window.get_size();
+        if width != self.framebuffer.width() || height != self.framebuffer.height() {
+            self.framebuffer = Framebuffer::new(width, height);
+        }
+    }
+
+    pub fn framebuffer(&mut self) -> &mut Framebuffer {
+        &mut self.framebuffer
     }
 }
-impl Framebuffer{
-    pub fn new_frame_buffer(width : usize, height : usize) -> Self{
-        Framebuffer{
-            data:
-            vec![0; width * height],
+
+impl Framebuffer {
+    pub fn new(width: usize, height: usize) -> Self {
+        Framebuffer {
+            data: vec![0; width * height],
             width,
             height
         }
     }
 
-    pub fn width(&self) -> usize{
+    pub fn width(&self) -> usize {
         self.width
     }
 
-    pub fn height(&self) -> usize{
+    pub fn height(&self) -> usize {
         self.height
     }
 
-    pub fn set_pixel(&mut self, x:usize, y:usize, value: u32){
+    pub fn set_pixel(&mut self, x: usize, y: usize, value: u32) {
         self.data[x + y * self.width] = value;
     }
 
-
+    pub fn clear(&mut self, value: u32) {
+        for i in 0..self.data.len() {
+            self.data[i] = value;
+        }
+    }
 }
